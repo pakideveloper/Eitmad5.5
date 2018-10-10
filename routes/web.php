5c634cbbef509419fdd5a3d5340536763fd9723e
pakideveloper <ilovepakistan811@gmail.com>
2018-10-10 13:56:01 +0500

    authentication updated

--- routes/web.php
+++ routes/web.php
@@ -10,94 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-//usama
 Route::get('/', function () {
     return view('frontend/general/index');
 });
 
-Route::get('/admin', function () {
-    return view('admin/ecommerce/modules/index');
-});
-Route::get('/admin2', function () {
-    return view('admin/ecommerce/modules/index2');
-}); 
-
-
-// Route::get('/ecommerce', function () {
-//     return view('frontend/ecommerce/modules/index');
-// });
-
-// Route::group(['middleware'=>'role:admin'], function(){
-
-// });
-
-
-// Route::get('/viewcategory', function () {
-//     return view('admin/ecommerce/modules/categories/viewCategories');
-// });
-
-
-
-// fetching cities on selecting country
-
-// Route::get('/register', function () {
-//     return view('frontend/ecommerce/modules/registration/register');
-// });
-
-// Route::get('/Home', function () {
-//     return view('index');
-// });
- // Route::get('/register', 'Auth\RegisterController@register');
- Route::get('/login', 'Auth\LoginController@login');
 Auth::routes();
-
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-
-
-//////////////////JOB PORTAL LINKS//////////////////
-
-///frontend links//
-
-// Route::get('/ecommerce', function () {
-//     return view('frontend/ecommerce/modules/index');
-// });
-
-
-// Route::get('/ecommerce', function () {
-//     return view('frontend/ecommerce/modules/index');
-// });
-
 
 Route::get('/home', function () {
     return view('frontend/general/index');
 });
-
-// Route::get('/Ecommerce', function () {
-//     return view('frontend/ecommerce/modules/index');
-// });
-
-
-
-
-
-// Route::get('/jobportal', function () {
-//     return view('frontend/JobPortal/index');
-// });
-
-// Route::get('/employers', function () {
-//     return view('frontend/JobPortal/pages/employers');
-// });
-// Route::get('/employerdetail', function () {
-//     return view('frontend/JobPortal/pages/employerdetail');
-// });
-// Route::get('/aboutportal', function () {
-//     return view('frontend/JobPortal/pages/AboutPortal');
-// });
-// Route::get('/contactus', function () {
-//     return view('frontend/JobPortal/pages/ContactUs');
-// });
-// Route::get('/addjob', function () {
-//     return view('frontend/JobPortal/pages/AddJob');
-// });
