1bbbe325075eff4060cb9aee95246046c44c527b
JJ <junaidjaved8461@gmail.com>
2018-10-20 10:51:41 +0500

    Brand logo fetching done with size and circle img

--- routes/j-admin.php
+++ routes/j-admin.php
@@ -16,11 +16,6 @@ Route::get('/', function () {
 });
 Route::resource('/degree_levels','admin\job\DegreeLevelController');
 Route::resource('/degree_types','admin\job\DegreeTypeController');
-<<<<<<< HEAD
 Route::resource('/industries','admin\job\IndustryController');
-=======
-
-///////////COMPANIES///////////
 Route::resource('companies','admin\job\AdminCompanyController');
 Route::post('companies/updateStatus','admin\job\AdminCompanyController@UpdateStatus');
->>>>>>> d981d22a5f5d796e03232e04a1ef4100cc6e82a0
