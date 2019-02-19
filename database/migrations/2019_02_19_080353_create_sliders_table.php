882773071392877c2e12c304025249a19899549a
pakideveloper <ilovepakistan811@gmail.com>
2019-02-19 13:05:30 +0500

    changes

---
+++ database/migrations/2019_02_19_080353_create_sliders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSlidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('titimage_namele');
+            $table->string('image_url');
+            $table->string('image_size');
+            $table->string('image_extension');
+            $table->string('slider_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sliders');
+    }
+}
